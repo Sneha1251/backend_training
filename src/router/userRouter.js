c515842f2694adac374b7936f0acc248680439f8
Sneha/successive <sneha.gupta@successive.tech>
2024-05-03 11:09:47 +0530

    Assignment-5

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -18,6 +18,23 @@ app.post("/query/:id", queryValidation, (req, res) => {
   res.json({ message: `user ${req.params.id}` });
 });
 
+app.get("/async-error", async (req, res, next) => {
+  try {
+    throw new Error("Async Error");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/validation/error/:id", (req, res, next) => {
+  const { id } = req.params;
+  if (!id || isNaN(id)) {
+    res.json("Id must be a number");
+  } else {
+    res.status(200).json({ message: "User details retreived successfully" });
+  }
+});
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
