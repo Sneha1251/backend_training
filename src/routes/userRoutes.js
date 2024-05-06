01fa9f9815f81434cedde43db27f744f00674006
Sneha/successive <sneha.gupta@successive.tech>
2024-05-06 15:00:55 +0530

    testing

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -16,6 +16,7 @@ const requestLogger = require("../middlewares/logger");
 const errorHandler = require("../middlewares/errorHandler");
 const customHeader = require("../middlewares/customHeader");
 const rateLimiter = require("../middlewares/rateLimiter");
+const validationError = require("../middlewares/validationError");
 
 const {
   middleware1,
@@ -74,6 +75,18 @@ app.get("/middleware", middleware1, middleware2, middleware3, (req, res) => {
 
 app.get("/error-500", (req, res, next) => {
   next(createError(500, "Internal server Error"));
+});
+
+app.get("/async-error", async (req, res, next) => {
+  try {
+    throw new Error("Async Error");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/validation/error/:id", validationError, (req, res) => {
+  res.status(200).json({ message: "User details retreived successfully" });
 });
 
 app.use(errorHandler);
