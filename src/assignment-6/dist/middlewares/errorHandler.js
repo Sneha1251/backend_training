fec9a1a21cd659d9ea6ddcf7b2125ce4a1826dbd
Sneha/successive <sneha.gupta@successive.tech>
2024-05-10 11:22:18 +0530

    js file updated

--- src/assignment-6/dist/middlewares/errorHandler.js
+++ src/assignment-6/dist/middlewares/errorHandler.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const errorHandler = (err, req, res, next) => {
-    console.error(err);
     return res.status(500).json({ error: "Internal Server Error" });
 };
 exports.default = errorHandler;
--- src/assignment-6/dist/middlewares/validateRequest.js
+++ src/assignment-6/dist/middlewares/validateRequest.js
@@ -1,14 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// interface ValidationSchema {
-//   [key: string]: {
-//     validate: (data: any) => { error?: string };
-//   };
-// }
-const userSchema = require("../validationRules");
+const validationRules_1 = __importDefault(require("../validationRules"));
 const validateRequest = (req, res, next) => {
-    const route = req.originalUrl;
-    const { error } = userSchema[route].validate(req.body);
+    const { error } = validationRules_1.default.validate(req.body);
     if (error) {
         return res.status(400).json({ error: error.details[0].message });
     }
