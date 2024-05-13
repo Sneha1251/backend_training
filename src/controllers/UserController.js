58effbb624ddc5fe33c4b6063139508b7e8caf86
Sneha/successive <sneha.gupta@successive.tech>
2024-05-13 14:08:47 +0530

    Assignment-11

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,11 @@
-require("../assignment-10/config/db.js");
+const db = require("../assignment-10/config/db.js");
 const customerSchema = require("../models/customerSchema.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const signup = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, roles } = req.body;
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -13,6 +13,7 @@ const signup = async (req, res) => {
     username: username,
     email: email,
     password: hashedPassword,
+    roles: roles,
   });
 
   if (newUser) {
@@ -26,6 +27,10 @@ const login = async (req, res) => {
   const { username, email, password } = req.body;
 
   const user = await customerSchema.findOne({ username: username });
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
 
   const validPassword = await bcrypt.compare(password, user.password);
 
@@ -41,8 +46,33 @@ const login = async (req, res) => {
   }
 };
 
-const getData = (req, res) => {
-  res.send(`${req.user.user.username}, You are validate`);
+const dashboard = (req, res) => {
+  res.send(
+    `${req.user.user.username}, Welcome to dashboard!  You are valid user`
+  );
 };
 
-module.exports = { signup, login, getData };
+const isAdmin = async (req, res) => {
+  const { password, email } = req.body;
+  try {
+    const user = await customerSchema.findOne({ email });
+
+    if (!user) {
+      return res.status(403).send("Invalid credentials!");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(403).send("Invalid Password!");
+    }
+
+    if (user.roles !== "admin") {
+      return res.status(403).send("Access Denied, Admin privileges required!");
+    }
+    return res.status(200).send("Admin! You are Authorised for data ");
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+};
+
+module.exports = { signup, login, dashboard, isAdmin };
--- src/routes/otherRoutes.js
+++ src/routes/otherRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
-const { login, signup, getData } = require("../controllers/UserController");
+const {
+  login,
+  signup,
+  dashboard,
+  isAdmin,
+} = require("../controllers/UserController");
 const jwtAuthentication = require("../middlewares/jwtAuthentication");
 
 const app = express();
@@ -10,7 +15,9 @@ app.post("/customer/signup", signup);
 
 app.post("/customer/login", login);
 
-app.get("/customer/data", jwtAuthentication, getData);
+app.get("/customer/dashboard", jwtAuthentication, dashboard);
+
+app.post("/customer/admin", isAdmin);
 
 app.listen(5000, () => {
   console.log("server is running on 5000 port");
