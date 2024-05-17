8faa97a82f4c856b4b228db4b470c7daf2988009
Sneha/successive <sneha.gupta@successive.tech>
2024-05-17 12:01:54 +0530

    Assignment-12

--- src/routes/otherRoutes.js
+++ src/routes/otherRoutes.js
@@ -3,9 +3,12 @@ const express = require("express");
 const commerce = require("../controllers/UserController.js");
 const commerceValidation = require("../middlewares/commerceValidation.js");
 const country = require("../controllers/UserController.js");
+const security = require("../middlewares/security.js");
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(security.setHeaders);
 
 app.post("/commerce/post", commerceValidation, commerce.userInsert);
 
